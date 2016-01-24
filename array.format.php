6bff010a544ca8a1f879097d015f2558245491d4
itcoder <prototypenk@gmail.com>
2016-01-24 19:46:45 +0300

    add array format

---
+++ array.format.php
@@ -0,0 +1,127 @@
+array(2) {
+  [0]=>
+  array(2) {
+    ["name"]=>
+    string(12) "activities-1"
+    ["clients"]=>
+    array(2) {
+      [0]=>
+      array(7) {
+        ["active"]=>
+        bool(true)
+        ["name"]=>
+        string(10) "Selfridges"
+        ["country"]=>
+        string(6) "Russia"
+        ["users_count"]=>
+        int(50)
+        ["money_spend"]=>
+        int(3000)
+        ["money_win"]=>
+        int(9000)
+        ["money_proffit"]=>
+        int(6000)
+      }
+      [1]=>
+      array(7) {
+        ["active"]=>
+        bool(false)
+        ["name"]=>
+        string(10) "Selfridges"
+        ["country"]=>
+        string(3) "Usa"
+        ["users_count"]=>
+        int(60)
+        ["money_spend"]=>
+        int(3000)
+        ["money_win"]=>
+        int(9000)
+        ["money_proffit"]=>
+        int(6000)
+      }
+    }
+  }
+  [1]=>
+  array(2) {
+    ["name"]=>
+    string(12) "activities-2"
+    ["clients"]=>
+    array(1) {
+      [0]=>
+      array(7) {
+        ["active"]=>
+        bool(true)
+        ["name"]=>
+        string(6) "Yandex"
+        ["country"]=>
+        string(6) "Russia"
+        ["users_count"]=>
+        int(9000)
+        ["money_spend"]=>
+        int(3000)
+        ["money_win"]=>
+        int(9000)
+        ["money_proffit"]=>
+        int(6000)
+      }
+    }
+  }
+
+
+  ------------------------------------------
+
+Array
+(
+    [0] => Array
+        (
+            [name] => activities-1
+            [clients] => Array
+                (
+                    [0] => Array
+                        (
+                            [active] => 1
+                            [name] => Selfridges
+                            [country] => Russia
+                            [users_count] => 50
+                            [money_spend] => 3000
+                            [money_win] => 9000
+                            [money_proffit] => 6000
+                        )
+
+                    [1] => Array
+                        (
+                            [active] => 
+                            [name] => Selfridges
+                            [country] => Usa
+                            [users_count] => 60
+                            [money_spend] => 3000
+                            [money_win] => 9000
+                            [money_proffit] => 6000
+                        )
+
+                )
+
+        )
+
+    [1] => Array
+        (
+            [name] => activities-2
+            [clients] => Array
+                (
+                    [0] => Array
+                        (
+                            [active] => 1
+                            [name] => Yandex
+                            [country] => Russia
+                            [users_count] => 9000
+                            [money_spend] => 3000
+                            [money_win] => 9000
+                            [money_proffit] => 6000
+                        )
+
+                )
+
+        )
+
+)
+
